6b6cb3ac91538e96f08feae9d22ae42ef198c779
Luís Eduardo Souza Barros <30378266+LudwigEdward@users.noreply.github.com>
2020-09-07 19:26:17 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ReactComponent as Logo } from './logo.svg';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo style={{ width: '128px', height: '128px' }} />
+        <p>
+          <code>React ESLint Boilerplate</code>
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
