a3c70f4ac1e14b90bbc84395b497f9475cd772ac
Luís Eduardo <le.barros20166@hotmail.com>
2020-09-24 08:33:45 -0400

    feature: finalizando estrutura de filme principal

--- src/Components/FeaturedMovie/index.tsx
+++ src/Components/FeaturedMovie/index.tsx
@@ -20,8 +20,6 @@ interface FeatureMovieProps{
 }
 
 const FeaturedMovie: React.FC<FeatureMovieProps> = ({item}) => {
-  console.log(item);
-
   const MovieDate =  new Date(item.first_air_date);
   return (
     <Container>
@@ -30,15 +28,16 @@ const FeaturedMovie: React.FC<FeatureMovieProps> = ({item}) => {
           <FeaturedFadeHorizontal>
             <FeaturedName>{item.original_name}</FeaturedName>
             <FeaturedInfo>
-                <FeaturedPoints>{item.vote_average * 10} %</FeaturedPoints>
+                <FeaturedPoints>{item.vote_average * 10}%</FeaturedPoints>
   <FeaturedYear>{MovieDate.getFullYear()}</FeaturedYear>
                 <FeaturedSeasons>{item.number_of_seasons} temporada{item.number_of_seasons > 1 ? "s":  ""}</FeaturedSeasons>
             </FeaturedInfo>
             <FeaturedDescription>{item.overview}</FeaturedDescription>
               <FeaturedButtons>
-
+                  <a href={`/watch/${item.id}`} className="featured-watchbutton">▶ Assistir</a>
+                  <a href={`/list/add/${item.id}`} className="featured-mylistbutton">+ Minha Lista</a>
               </FeaturedButtons>
-  <FeaturedGenres><strong>Generos:</strong> {item.genres.map((genre:Record<string,any>)=>(<span>{genre.name}</span>))}</FeaturedGenres>
+  <FeaturedGenres><strong>Generos:</strong> {item.genres.map((genre:Record<string,any>,index:number)=>(<span>{genre.name}{index+1 === item.genres.length ? " ": ", "}</span>))}</FeaturedGenres>
           </FeaturedFadeHorizontal>
         </FeaturedFadeVertial>
       </Featured>
--- src/Components/FeaturedMovie/styles.ts
+++ src/Components/FeaturedMovie/styles.ts
@@ -9,7 +9,7 @@ export const Container = styled.div`
 `;
 
 export const Featured = styled.div`
-  height:90vh;
+  height:100vh;
   background-size: cover;
   background-position: center;
   background-image:url(https://image.tmdb.org/t/p/original${(props:FeaturedImageProps)=> props.backdrop_path});
@@ -25,15 +25,75 @@ export const FeaturedFadeHorizontal = styled.div`
  width:inherit;
  height:inherit;
  background:linear-gradient(to right, rgba(17, 17, 17,0.95) 30%, transparent 70%);
+ display:flex;
+ flex-direction:column;
+ justify-content:center;
+ padding-left:30px;
+ padding-bottom:150px;
+ padding-top:70px;
 `;
 
 
-export const FeaturedSeasons = styled.div``;
-  export const FeaturedInfo = styled.div``;
-  export const FeaturedPoints = styled.div``;
-  export const FeaturedName = styled.div``;
-  export const FeaturedYear = styled.div``;
+export const FeaturedSeasons = styled.div`
+  display:inline-block;
+  margin-right:15px;
+`;
+  export const FeaturedInfo = styled.div`
+    font-size:18px;
+    font-weight:bold;
+    margin-top:15px;
+  `;
+  export const FeaturedPoints = styled.div`
+    display:inline-block;
+    color:#46d369;
+    margin-right:15px;
+  `;
+  export const FeaturedName = styled.div`
+    font-size:60px;
+    font-weight:bold;
+  `;
+  export const FeaturedYear = styled.div`
+    display:inline-block;
+    margin-right:15px;
 
-  export const FeaturedDescription = styled.div``;
-  export const FeaturedButtons = styled.div``;
-  export const FeaturedGenres = styled.div``;
+  `;
+
+  export const FeaturedDescription = styled.div`
+    margin-top:15px;
+    font-size:20px;
+    color:#999;
+    max-width:35%;
+  `;
+  export const FeaturedButtons = styled.div`
+    margin: 15px 0;
+  a{
+     display:inline-block;
+      font-size:20px;
+      font-weight:bold;
+      padding: 12px 25px;
+      border-radius:5px;
+      text-decoration:none;
+      margin-right:10px;
+      opacity:1;
+      transition: 0.5s ease;
+  }
+  a:hover{
+    opacity:0.8;
+  }
+
+    .featured-mylistbutton{
+    background-color: #333;
+    color:#FFF;
+    }
+   .featured-watchbutton{
+    background-color: #fff;
+    color:#000;
+   }
+  `;
+  export const FeaturedGenres = styled.div`
+    margin-top:15px;
+    font-size:18px;
+    color:#999;
+  `;
+
+
